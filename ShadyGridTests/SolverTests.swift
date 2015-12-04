e4344e1461878c7af4caf36aab3e5756165ba8cb
Toni Moeckel <tonimoeckel@gmail.com>
2015-12-04 22:46:30 +0100

    travis-setup

--- ShadyGridTests/SolverTests.swift
+++ ShadyGridTests/SolverTests.swift
@@ -13,11 +13,11 @@ class SolverTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
         super.tearDown()
     }
     
